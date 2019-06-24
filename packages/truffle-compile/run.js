c598c7c61d30884bed8671e73f94c1b5b22c6522
g. nicholas d'andrea <nick@gnidan.org>
2019-06-24 19:40:09 -0400

    Refactor compilation process
    - Convert `compile()` to async
    
    - Move the core implementation of `compile()` into a separate
      module/function "truffle-compile/run"
      - Separate discrete operations into functions, roughly organized
        by input and output
      - Replace for loops / object mutation pattern with map/filter/reduce
      - In particular, get rid of return contract object mutation with
        chained, functional data processing
      - De-dupe link refs. with new `replaceAllLinkReferences()`
      - Use camelCase for variable names where applicable
    
    - Start redefining truffle-compile's return format. Define `run` to
      return new format:
        - Return `contractName` instead of `contract_name`
        - Remove `unlinked_binary`
        - Return an object with results { contracts, sourceIndex, compilerInfo }
          where `contracts` is a list, not a name mapping.
    
    - Maintain legacy callback interface with new module "truffle-compile/legacy"
      - Move options-normalization into "truffle-compile/legacy/options"
      - Create shim from new output format to legacy return values

--- packages/truffle-compile/run.js
+++ packages/truffle-compile/run.js
@@ -4,9 +4,12 @@ const CompilerSupplier = require("./compilerSupplier");
 const semver = require("semver");
 
 async function run(rawSources, options) {
-  // Nothing to compile? Bail.
   if (Object.keys(rawSources).length === 0) {
-    return [[], []];
+    return {
+      contracts: [],
+      sourceIndexes: [],
+      compilerInfo: undefined
+    };
   }
 
   // Ensure sources have operating system independent paths
@@ -16,230 +19,54 @@ async function run(rawSources, options) {
     options.compilationTargets
   );
 
-  const defaultSelectors = {
-    "": ["legacyAST", "ast"],
-    "*": [
-      "abi",
-      "metadata",
-      "evm.bytecode.object",
-      "evm.bytecode.sourceMap",
-      "evm.deployedBytecode.object",
-      "evm.deployedBytecode.sourceMap",
-      "userdoc",
-      "devdoc"
-    ]
-  };
-
-  // Specify compilation targets
-  // Each target uses defaultSelectors, defaulting to single target `*` if targets are unspecified
-  const outputSelection = {};
-
-  targets.length
-    ? targets.forEach(key => (outputSelection[key] = defaultSelectors))
-    : (outputSelection["*"] = defaultSelectors);
-
-  const solcStandardInput = {
-    language: "Solidity",
-    sources: {},
-    settings: {
-      evmVersion: options.compilers.solc.settings.evmVersion,
-      optimizer: options.compilers.solc.settings.optimizer,
-      outputSelection
-    }
-  };
-
-  Object.keys(sources).forEach(file_path => {
-    solcStandardInput.sources[file_path] = {
-      content: sources[file_path]
-    };
+  // construct solc compiler input
+  const compilerInput = prepareCompilerInput({
+    sources,
+    targets,
+    settings: options.compilers.solc.settings
   });
 
-  // Load solc module only when compilation is actually required.
-  const supplier = new CompilerSupplier(options.compilers.solc);
-  const solc = await supplier.load();
+  // perform compilation
+  const { compilerOutput, solcVersion } = await invokeCompiler({
+    compilerInput,
+    options
+  });
 
-  const result = solc.compile(JSON.stringify(solcStandardInput));
-
-  const standardOutput = JSON.parse(result);
-
-  let errors = standardOutput.errors || [];
-  let warnings = [];
-
-  if (options.strict !== true) {
-    warnings = errors.filter(error => error.severity === "warning");
-
-    errors = errors.filter(error => error.severity !== "warning");
-
-    if (options.quiet !== true && warnings.length > 0) {
-      options.logger.log(
-        OS.EOL + "    > compilation warnings encountered:" + OS.EOL
-      );
-      options.logger.log(
-        warnings.map(warning => warning.formattedMessage).join()
-      );
-    }
+  // handle warnings as errors if options.strict
+  // log if not options.quiet
+  const { warnings, errors } = detectErrors({ compilerOutput, options });
+  if (warnings.length > 0 && !options.quiet) {
+    options.logger.log(
+      OS.EOL + "    > compilation warnings encountered:" + OS.EOL
+    );
+    options.logger.log(warnings);
   }
 
   if (errors.length > 0) {
-    options.logger.log("");
-    errors = errors.map(error => error.formattedMessage).join();
-    if (errors.includes("requires different compiler version")) {
-      const contractSolcVer = errors.match(/pragma solidity[^;]*/gm)[0];
-      const configSolcVer =
-        options.compilers.solc.version || semver.valid(solc.version());
-      errors = errors.concat(
-        `\nError: Truffle is currently using solc ${configSolcVer}, but one or more of your contracts specify "${contractSolcVer}".\nPlease update your truffle config or pragma statement(s).\n(See https://truffleframework.com/docs/truffle/reference/configuration#compiler-configuration for information on\nconfiguring Truffle to use a specific solc compiler version.)`
-      );
+    if (!options.quiet) {
+      options.logger.log("");
     }
+
     throw new CompileError(errors);
   }
 
-  var files = [];
-  Object.keys(standardOutput.sources).forEach(filename => {
-    var source = standardOutput.sources[filename];
-    files[source.id] = originalSourcePaths[filename];
-  });
-
-  var returnVal = {};
-
-  // This block has comments in it as it's being prepared for solc > 0.4.10
-  Object.entries(standardOutput.contracts).forEach(entry => {
-    const [sourcePath, filesContracts] = entry;
-
-    Object.entries(filesContracts).forEach(entry => {
-      var [contractName, contract] = entry;
-
-      // All source will have a key, but only the compiled source will have
-      // the evm output.
-      if (!Object.keys(contract.evm).length) return;
-
-      var contract_definition = {
-        contract_name: contractName,
-        sourcePath: originalSourcePaths[sourcePath], // Save original source path, not modified ones
-        source: sources[sourcePath],
-        sourceMap: contract.evm.bytecode.sourceMap,
-        deployedSourceMap: contract.evm.deployedBytecode.sourceMap,
-        legacyAST: standardOutput.sources[sourcePath].legacyAST,
-        ast: standardOutput.sources[sourcePath].ast,
-        abi: contract.abi,
-        metadata: contract.metadata,
-        bytecode: "0x" + contract.evm.bytecode.object,
-        deployedBytecode: "0x" + contract.evm.deployedBytecode.object,
-        unlinked_binary: "0x" + contract.evm.bytecode.object, // deprecated
-        compiler: {
-          name: "solc",
-          version: solc.version()
-        },
-        devdoc: contract.devdoc,
-        userdoc: contract.userdoc
-      };
-
-      // Reorder ABI so functions are listed in the order they appear
-      // in the source file. Solidity tests need to execute in their expected sequence.
-      contract_definition.abi = orderABI(contract_definition);
-
-      // Go through the link references and replace them with older-style
-      // identifiers. We'll do this until we're ready to making a breaking
-      // change to this code.
-      Object.keys(contract.evm.bytecode.linkReferences).forEach(function(
-        file_name
-      ) {
-        var fileLinks = contract.evm.bytecode.linkReferences[file_name];
-
-        Object.keys(fileLinks).forEach(function(library_name) {
-          var linkReferences = fileLinks[library_name] || [];
-
-          contract_definition.bytecode = replaceLinkReferences(
-            contract_definition.bytecode,
-            linkReferences,
-            library_name
-          );
-          contract_definition.unlinked_binary = replaceLinkReferences(
-            contract_definition.unlinked_binary,
-            linkReferences,
-            library_name
-          );
-        });
-      });
-
-      // Now for the deployed bytecode
-      Object.keys(contract.evm.deployedBytecode.linkReferences).forEach(
-        function(file_name) {
-          var fileLinks =
-            contract.evm.deployedBytecode.linkReferences[file_name];
-
-          Object.keys(fileLinks).forEach(function(library_name) {
-            var linkReferences = fileLinks[library_name] || [];
-
-            contract_definition.deployedBytecode = replaceLinkReferences(
-              contract_definition.deployedBytecode,
-              linkReferences,
-              library_name
-            );
-          });
-        }
-      );
-
-      returnVal[contractName] = contract_definition;
-    });
-  });
-
-  const compilerInfo = { name: "solc", version: solc.version() };
-
-  return [returnVal, files, compilerInfo];
-}
-
-function replaceLinkReferences(bytecode, linkReferences, libraryName) {
-  var linkId = "__" + libraryName;
-
-  while (linkId.length < 40) {
-    linkId += "_";
-  }
-
-  linkReferences.forEach(function(ref) {
-    // ref.start is a byte offset. Convert it to character offset.
-    var start = ref.start * 2 + 2;
-
-    bytecode =
-      bytecode.substring(0, start) + linkId + bytecode.substring(start + 40);
-  });
-
-  return bytecode;
-}
-
-function orderABI({ abi, contract_name: contractName, ast }) {
-  // AST can have multiple contract definitions, make sure we have the
-  // one that matches our contract
-  const contractDefinition = ast.nodes.find(
-    ({ nodeType, name }) =>
-      nodeType === "ContractDefinition" && name === contractName
-  );
-
-  if (!contractDefinition || !contractDefinition.nodes) {
-    return abi;
-  }
-
-  // Find all function definitions
-  const orderedFunctionNames = contractDefinition.nodes
-    .filter(({ nodeType }) => nodeType === "FunctionDefinition")
-    .map(({ name: functionName }) => functionName);
-
-  // Put function names in a hash with their order, lowest first, for speed.
-  const functionIndexes = orderedFunctionNames
-    .map((functionName, index) => ({ [functionName]: index }))
-    .reduce((a, b) => Object.assign({}, a, b), {});
-
-  // Construct new ABI with functions at the end in source order
-  return [
-    ...abi.filter(({ name }) => functionIndexes[name] === undefined),
-
-    // followed by the functions in the source order
-    ...abi
-      .filter(({ name }) => functionIndexes[name] !== undefined)
-      .sort(
-        ({ name: a }, { name: b }) => functionIndexes[a] - functionIndexes[b]
-      )
-  ];
+  // success case
+  return {
+    sourceIndexes: processSources({
+      compilerOutput,
+      originalSourcePaths
+    }),
+    contracts: processContracts({
+      sources,
+      compilerOutput,
+      solcVersion,
+      originalSourcePaths
+    }),
+    compilerInfo: {
+      name: "solc",
+      version: solcVersion
+    }
+  };
 }
 
 /**
@@ -288,6 +115,311 @@ function collectSources(originalSources, originalTargets = []) {
     }),
     defaultAccumulator
   );
+}
+
+/**
+ * @param sourcePath - string
+ * @return string - operating system independent path
+ * @private
+ */
+function getPortableSourcePath(sourcePath) {
+  // Turn all backslashes into forward slashes
+  var replacement = sourcePath.replace(/\\/g, "/");
+
+  // Turn G:/.../ into /G/.../ for Windows
+  if (replacement.length >= 2 && replacement[1] === ":") {
+    replacement = "/" + replacement;
+    replacement = replacement.replace(":", "");
+  }
+
+  return replacement;
+}
+
+/**
+ *
+ */
+function prepareCompilerInput({ sources, targets, settings }) {
+  return {
+    language: "Solidity",
+    sources: prepareSources({ sources }),
+    settings: {
+      evmVersion: settings.evmVersion,
+      optimizer: settings.optimizer,
+      outputSelection: prepareOutputSelection({ targets })
+    }
+  };
+}
+
+function prepareSources({ sources }) {
+  return Object.entries(sources)
+    .map(([sourcePath, content]) => ({ [sourcePath]: { content } }))
+    .reduce((a, b) => Object.assign({}, a, b), {});
+}
+
+const defaultSelectors = {
+  "": ["legacyAST", "ast"],
+  "*": [
+    "abi",
+    "metadata",
+    "evm.bytecode.object",
+    "evm.bytecode.sourceMap",
+    "evm.deployedBytecode.object",
+    "evm.deployedBytecode.sourceMap",
+    "userdoc",
+    "devdoc"
+  ]
+};
+
+/**
+ * If targets are specified, specify output selectors fo each individually.
+ * Otherwise, just use "*" selector
+ */
+function prepareOutputSelection({ targets = [] }) {
+  if (!targets.length) {
+    return {
+      "*": defaultSelectors
+    };
+  }
+
+  return targets
+    .map(target => ({ [target]: defaultSelectors }))
+    .reduce((a, b) => Object.assign({}, a, b), {});
+}
+
+/**
+ * Load solc and perform compilation
+ */
+async function invokeCompiler({ compilerInput, options }) {
+  // load solc
+  const supplier = new CompilerSupplier(options.compilers.solc);
+  const solc = await supplier.load();
+  const solcVersion = solc.version();
+
+  // perform compilation
+  const inputString = JSON.stringify(compilerInput);
+  const outputString = solc.compile(inputString);
+  const compilerOutput = JSON.parse(outputString);
+
+  return {
+    compilerOutput,
+    solcVersion
+  };
+}
+
+/**
+ * Extract errors/warnings from compiler output based on strict mode setting
+ * @return { errors: string, warnings: string }
+ */
+function detectErrors({ compilerOutput: { errors: outputErrors }, options }) {
+  outputErrors = outputErrors || [];
+  const rawErrors = options.strict
+    ? outputErrors
+    : outputErrors.filter(({ severity }) => severity !== "warning");
+
+  const rawWarnings = options.strict
+    ? [] // none of those in strict mode
+    : outputErrors.filter(({ severity }) => severity === "warning");
+
+  // extract messages
+  let errors = rawErrors.map(({ formattedMessage }) => formattedMessage).join();
+  const warnings = rawWarnings
+    .map(({ formattedMessage }) => formattedMessage)
+    .join();
+
+  if (errors.includes("requires different compiler version")) {
+    const contractSolcVer = errors.match(/pragma solidity[^;]*/gm)[0];
+    const configSolcVer =
+      options.compilers.solc.version || semver.valid(solc.version());
+
+    errors = errors.concat(
+      [
+        OS.EOL,
+        `Error: Truffle is currently using solc ${configSolcVer}, `,
+        `but one or more of your contracts specify "${contractSolcVer}".`,
+        OS.EOL,
+        `Please update your truffle config or pragma statement(s).`,
+        OS.EOL,
+        `(See https://truffleframework.com/docs/truffle/reference/configuration#compiler-configuration `,
+        `for information on`,
+        OS.EOL,
+        `configuring Truffle to use a specific solc compiler version.)`
+      ].join("")
+    );
+  }
+
+  return { warnings, errors };
+}
+
+/**
+ * Aggregate list of sources based on reported source index
+ * Returns list transformed to use original source paths
+ */
+function processSources({ compilerOutput, originalSourcePaths }) {
+  let files = [];
+
+  for (let [sourcePath, { id }] of Object.entries(compilerOutput.sources)) {
+    files[id] = originalSourcePaths[sourcePath];
+  }
+
+  return files;
+}
+
+/**
+ * Converts compiler-output contracts into truffle-compile's return format
+ * Uses compiler contrarct output plus other information.
+ */
+function processContracts({
+  compilerOutput,
+  sources,
+  originalSourcePaths,
+  solcVersion
+}) {
+  return (
+    Object.entries(compilerOutput.contracts)
+      // map to [[{ source, contractName, contract }]]
+      .map(([sourcePath, sourceContracts]) =>
+        Object.entries(sourceContracts).map(([contractName, contract]) => ({
+          contractName,
+          contract,
+          source: {
+            ast: compilerOutput.sources[sourcePath].ast,
+            legacyAST: compilerOutput.sources[sourcePath].legacyAST,
+            contents: sources[sourcePath],
+            sourcePath
+          }
+        }))
+      )
+      // and flatten
+      .reduce((a, b) => [...a, ...b], [])
+
+      // All source will have a key, but only the compiled source will have
+      // the evm output.
+      .filter(({ contract: { evm } }) => Object.keys(evm).length > 0)
+
+      // convert to output format
+      .map(
+        ({
+          contractName,
+          contract: {
+            evm: {
+              bytecode: { sourceMap, linkReferences, object: bytecode },
+              deployedBytecode: {
+                sourceMap: deployedSourceMap,
+                linkReferences: deployedLinkReferences,
+                object: deployedBytecode
+              }
+            },
+            abi,
+            metadata,
+            devdoc,
+            userdoc
+          },
+          source: {
+            ast,
+            legacyAST,
+            sourcePath: transformedSourcePath,
+            contents: source
+          }
+        }) => ({
+          contractName,
+          abi: orderABI({ abi, contractName, ast }),
+          metadata,
+          devdoc,
+          userdoc,
+          sourcePath: originalSourcePaths[transformedSourcePath],
+          source,
+          sourceMap,
+          deployedSourceMap,
+          ast,
+          legacyAST,
+          bytecode: replaceAllLinkReferences({
+            bytecode,
+            linkReferences
+          }),
+          deployedBytecode: replaceAllLinkReferences({
+            bytecode: deployedBytecode,
+            linkReferences: deployedLinkReferences
+          }),
+          compiler: {
+            name: "solc",
+            version: solcVersion
+          }
+        })
+      )
+  );
+}
+
+function orderABI({ abi, contractName, ast }) {
+  // AST can have multiple contract definitions, make sure we have the
+  // one that matches our contract
+  const contractDefinition = ast.nodes.find(
+    ({ nodeType, name }) =>
+      nodeType === "ContractDefinition" && name === contractName
+  );
+
+  if (!contractDefinition || !contractDefinition.nodes) {
+    return abi;
+  }
+
+  // Find all function definitions
+  const orderedFunctionNames = contractDefinition.nodes
+    .filter(({ nodeType }) => nodeType === "FunctionDefinition")
+    .map(({ name: functionName }) => functionName);
+
+  // Put function names in a hash with their order, lowest first, for speed.
+  const functionIndexes = orderedFunctionNames
+    .map((functionName, index) => ({ [functionName]: index }))
+    .reduce((a, b) => Object.assign({}, a, b), {});
+
+  // Construct new ABI with functions at the end in source order
+  return [
+    ...abi.filter(({ name }) => functionIndexes[name] === undefined),
+
+    // followed by the functions in the source order
+    ...abi
+      .filter(({ name }) => functionIndexes[name] !== undefined)
+      .sort(
+        ({ name: a }, { name: b }) => functionIndexes[a] - functionIndexes[b]
+      )
+  ];
+}
+
+function replaceAllLinkReferences({ bytecode, linkReferences }) {
+  // convert to flat list
+  const libraryLinkReferences = Object.values(linkReferences)
+    .map(fileLinks =>
+      Object.entries(fileLinks).map(([libraryName, links]) => ({
+        libraryName,
+        links
+      }))
+    )
+    .reduce((a, b) => [...a, ...b], []);
+
+  const unprefixed = libraryLinkReferences.reduce(
+    (bytecode, { libraryName, links }) =>
+      replaceLinkReferences(bytecode, links, libraryName),
+    bytecode
+  );
+
+  return `0x${unprefixed}`;
+}
+
+function replaceLinkReferences(bytecode, linkReferences, libraryName) {
+  var linkId = "__" + libraryName;
+
+  while (linkId.length < 40) {
+    linkId += "_";
+  }
+
+  linkReferences.forEach(function(ref) {
+    // ref.start is a byte offset. Convert it to character offset.
+    var start = ref.start * 2 + 2;
+
+    bytecode =
+      bytecode.substring(0, start) + linkId + bytecode.substring(start + 40);
+  });
+
+  return bytecode;
 }
 
 /**
